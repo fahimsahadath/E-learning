e4ed1faaeb2b9e828cabac652ec3b0d4de6c5988
fahimsahadath <52478447+fahimsahadath@users.noreply.github.com>
2020-10-15 14:37:04 +0600

    first committ

---
+++ home.php
@@ -0,0 +1,35 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">
+<h4>About UAP</h4>
+    <p>
+
+
+     For more than a decade University of Asia Pacific has been functioning as a unique institution of higher learning. Its reputation has grown rapidly, as it has already achieved eminence above most of the seats of higher learning of the country. Its commitment to excellence is demonstrated through the quality of its academic services and its invaluable contribution to learning. With its eminent administrators and excellent faculty members it is assuming a leading role in the educational arena of Bangladesh.. </p>
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4>
+  <p>Vision
+UAP holds steadfastly its passion to do better and better in fulfilling our young generation's needs and aspirations for a caring and quality education in casting their future career and become a desirable destination for an identity. UAP holds steadfastly its passion to do better and better in fulfilling our young generation's needs and aspirations for a caring and quality education in casting their future career and become a desirable destination for an identity.</p>
+  <h4>Mission</h4>
+  <p>Mission
+UAP mission is to offer best possible education to our young generation. Towards the mission, UAP continues to develop a sustained culture of ascending to a top-tier of vibrant academic environment; maintain and foster well qualified faculty, provide adequate research support for cutting-edge research in-house and in collaboration national and international peers; update curricula to keep up with advancing trend in science and technology, use state-of-the-art best practices in teaching-learning and modern facilities in laboratories and libraries; and provide other supports in aid to students becoming competent graduates with their potential fully realized and personality well-developed for joining the global forces in making the future of society in changing world
+.</p>
+  <h4>Core Values</h4>
+  <ol>
+    <li>Pursuit of TRUTH.</li>
+    <li>Passion for EXCELLENCE</li>
+    <li>Commitment to SERVICE</li>
+  </ol>
+
+
+</div>
+<div  class="col-lg-6">
+  <h4>Guiding Principles</h4>
+    <ul>
+      <li>Academic Freedom </li>
+      <li>Responsibility</li>
+      <li>Academic Standards</li>
+    </ul>
+
+</div>
